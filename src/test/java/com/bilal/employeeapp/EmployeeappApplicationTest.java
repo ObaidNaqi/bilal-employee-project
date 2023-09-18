a00251eac3aec770c3d747fabda44f51d27185c4
iuzairsheikh <uzairsheikh595@gmail.com>
2023-09-18 19:02:24 +0500

    add assert in context load method

--- src/test/java/com/bilal/employeeapp/EmployeeappApplicationTest.java
+++ src/test/java/com/bilal/employeeapp/EmployeeappApplicationTest.java
@@ -1,14 +1,27 @@
 package com.bilal.employeeapp;
 
-import org.junit.jupiter.api.Test;
+
+
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-
-@SpringBootTest(classes = EmployeeManagementApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EmployeeManagementApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 public class EmployeeappApplicationTest {
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		assertNotEquals(null,restTemplate);
 	}
 
 }
